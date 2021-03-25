be8fd2e418eee60c5bd506a342de3168b572a993
deisaacs98 <deisaacs98@gmail.com>
2021-03-25 08:58:52 -0500

    Added properties to gardener model.

--- gardenhelper/models/gardener.py
+++ gardenhelper/models/gardener.py
@@ -4,13 +4,27 @@ from types import SimpleNamespace
 
 
 class gardener:
-    def __init__(self, _id, ):
+    def __init__(self, _id, _first_name, _middle_initial, _last_name, _email, _address_line1,_address_line2, _city,
+                 _state, _zip, _phone, _lat, _lng):
         self._id = _id
-
+        self.first_name = _first_name
+        self.middle_initial = _middle_initial
+        self.last_name = _last_name
+        self.email = _email
+        self.address_line1 = _address_line1
+        self.address_line2 = _address_line2
+        self.city = _city
+        self.state = _state
+        self.zip = _zip
+        self.phone = _phone
+        self.lat = _lat
+        self.lng = _lng
 
     @staticmethod
     def gardener_decoder(obj):
-        return gardener(obj['_id'])
+        return gardener(obj['_id'], obj['_first_name'], obj['_middle_initial'], obj['_last_name'], obj['_email'],
+                        obj['_address_line1'], obj['_address_line2'], obj['_city'],  obj['_state'],
+                        obj['_zip'], obj['_phone'], obj['_lat'], obj['_lng'])
 
 
 ##Not sure what I will be putting in here at the moment.
