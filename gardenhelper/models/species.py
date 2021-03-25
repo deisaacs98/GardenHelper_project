8f349539f2b6058c2c6f0292c5aa9956f8b6a332
deisaacs98 <deisaacs98@gmail.com>
2021-03-25 10:39:38 -0500

    Added growth model based on Trefle API. Do not plan on calling API here, as limiting API calls is worth the effort. However, the amount of data coming in seems to require separate models for ease of access.

--- gardenhelper/models/species.py
+++ gardenhelper/models/species.py
@@ -28,9 +28,10 @@ class Species:
 
     @staticmethod
     def gardener_decoder(obj):
-        return Species(obj['_id'], obj['common_name'],obj['_scientific_name'], obj['_status'],  obj['_rank'],
-                        obj['_family_common_name'], obj['_family'], obj['_genus_id'],  obj['_genus'],
-                        obj['_edible_part'], obj['_edible'], obj['_vegetable'], obj['_observations'])
+        return Species(obj['_id'], obj['_growth_id'], obj['_distribution_id'],  obj['_specifications_id'],
+                       obj['_images_id'], obj['_images_url'], obj['common_name'],obj['_scientific_name'],
+                       obj['_status'],  obj['_rank'], obj['_family_common_name'], obj['_family'], obj['_genus_id'],
+                       obj['_genus'], obj['_edible_part'], obj['_edible'], obj['_vegetable'], obj['_observations'])
 
 
 ##Not sure what I will be putting in here at the moment.
